b7bcecff2948302c727b2f29352bdb6698896b28
regina <reginakuzmiv310@gmail.com>
2026-05-29 15:53:35 +0200

    resolved some unfunctional problems

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -4,36 +4,60 @@ const formData = {
 };
 
 const form = document.querySelector('.feedback-form');
-const textarea = form.elements.message;
-const email = form.elements.email;
 
 const localStorageKey = 'feedback-form-state';
 
 form.addEventListener('input', e => {
-  const formData = {
-    email: email.value,
-    message: textarea.value,
-  };
+  const fd = new FormData(form);
+  formData.email = fd.get('email');
+  formData.message = fd.get('message');
 
   saveToLS(localStorageKey, formData);
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+  const data = loadFromLS(localStorageKey);
+
+  if (data) {
+    form.elements.email.value = data.email;
+    form.elements.message.value = data.message;
+
+    formData.email = data.email || '';
+    formData.message = data.message || '';
+  }
+});
+
 form.addEventListener('submit', e => {
   e.preventDefault();
-  const formData = {
-    email: email.value,
-    message: textarea.value,
+
+  const data = {
+    email: formData.email.trim(),
+    message: formData.message.trim(),
   };
-  if (formData.email === '' || formData.message === '') {
+
+  if (!data.email || !data.message) {
     alert(`Fill please all fields`);
+    return;
   }
 
-  console.log(formData);
+  console.log(data);
 
   localStorage.removeItem(localStorageKey);
   form.reset();
+
+  formData.email = '';
+  formData.message = '';
 });
 
 function saveToLS(key, value) {
-  const json = JSON.stringify(value);
-  localStorage.setItem(key, json);
+  localStorage.setItem(key, JSON.stringify(value));
+}
+
+function loadFromLS(key) {
+  const json = localStorage.getItem(key);
+  try {
+    return JSON.parse(json);
+  } catch {
+    return json;
+  }
 }
